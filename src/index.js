85ed80c49f22da21d7e35ef0aec5b825e139e075
Taz497 <tazimhasan678@gmail.com>
2023-01-14 14:25:59 +0600

    1.4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log("yo")
+// DOM elements
+
+let apps = document.getElementById("apps");
+let apps_container = document.getElementById("apps_container");
+let account = document.getElementById("account");
+let account_container = document.getElementById("account_container");
+let camera = document.getElementById("camera");
+let camera_icon = document.getElementById("camera_icon");
+
+// Show tooltip
+
+apps_container.addEventListener("mouseover", function show() {
+  apps.style.visibility = "visible";
+});
+
+account_container.addEventListener("mouseover", function show() {
+  account.style.visibility = "visible";
+});
+
+camera_icon.addEventListener("mouseover", function show() {
+  camera.style.visibility = "visible";
+});
+
+// Hide tooltip
+
+account_container.addEventListener("mouseleave", function hide() {
+  account.style.visibility = "hidden";
+});
+
+apps_container.addEventListener("mouseleave", function hide() {
+  apps.style.visibility = "hidden";
+});
+
+camera_icon.addEventListener("mouseleave", function hide() {
+  camera.style.visibility = "hidden";
+});
